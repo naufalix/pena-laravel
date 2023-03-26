fd5a3b9bc06b1fca073dd0e343e387eca652b8f0
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2023-03-27 00:11:48 +0700

    Remove links

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -57,9 +57,7 @@
   <div class="d-flex flex-center flex-column-auto p-10">
     <!--begin::Links-->
     <div class="d-flex align-items-center fw-bold fs-6">
-      <a href="https://keenthemes.com" class="text-muted text-hover-primary px-2">About</a>
-      <a href="mailto:support@keenthemes.com" class="text-muted text-hover-primary px-2">Contact</a>
-      <a href="https://keenthemes.com/products/ceres-html-pro" class="text-muted text-hover-primary px-2">Contact Us</a>
+      Created by <a href="https://naufal.dev" class="text-muted text-hover-primary px-2" target="_blank">naufal.dev</a>
     </div>
     <!--end::Links-->
   </div>
